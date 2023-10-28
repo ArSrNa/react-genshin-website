d45766b52a8104df1420e11a08780fc3843134bc
ArSrNa <arsrna.cn@outlook.com>
2023-10-28 17:23:33 +0800

    Frame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { Layout } from 'antd';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Liyue from './home';
+const { Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Main />
+    </Router>
   );
+}
+
+function Main() {
+  return (
+    <Layout>
+      <Header />
+      <Content>
+        <Routes>
+          <Route path="/" element={<Liyue />} />
+        </Routes>
+      </Content>
+    </Layout>
+  )
+}
+
+function Header() {
+  const menus = [{
+    label: "首页（没做）",
+    active: false
+  }, {
+    label: "角色",
+    active: true
+  }, {
+    label: "没做",
+    active: false
+  }, {
+    label: "没做",
+    active: false
+  }]
+  return (<>
+    <div className='header'>
+      <img style={{ height: 60 }} src={require('./images/header/header.png')} />
+      {menus.map(menu => (
+        <div key={(Math.random() * 1000).toFixed(2)} className={`nav-item ${menu.active ? 'nav-item-active' : ''}`}>{menu.label}</div>
+      ))}
+    </div>
+  </>)
 }
 
 export default App;
