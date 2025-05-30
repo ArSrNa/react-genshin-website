d43ebc235ad87054ebe66fd7d5b06b89adcaff5a
arsrna <root@arsrna.cn>
2025-05-30 14:38:37 +0800

    ci: 修改构建输出为静态导出并更新tsconfig目标版本

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: false,
     transpilePackages: ["antd", "@ant-design", "@ant-design/icons", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table"],
     // distDir: 'build',
-    // output: 'standalone',
+    output: 'export',
     // trailingSlash: true
 };
 
